08ac3cca04e2bfd6b99b5b3bed69ed97ff9d792d
preetaman1208 <preetaman1208@gmail.com>
2020-10-25 01:29:38 +0530

    Assets in office

--- script.js
+++ script.js
@@ -2,13 +2,35 @@ var ambientLight;
 var directionalLight;
 
 var sceneEl = document.querySelector('a-scene');
-var lightAmb = sceneEl.querySelector('a-light');
+//var lightAmb = sceneEl.querySelector('a-light');
+
+var eventEl = sceneEl.querySelector('a-entity');
+
+var animation__mouseEnter = document.createAttribute("animation__mouseEnter");
+var animation__mouseLeave = document.createAttribute("animation__mouseLeave");
+animation__mouseEnter.value ="property: object3D.position.x; to: 1; dir: alternate; dur: 3000; loop: 1; startEvents: mouseenter";
+animation__mouseLeave.value ="property: object3D.position.x; to: 0; dir: alternate; dur: 3000; loop: 1; startEvents: mouseleave";
+eventEl.setAttributeNode(animation__mouseEnter);
+eventEl.setAttributeNode(animation__mouseLeave);
+
+//animation="property: object3D.position.x; to: 1; dir: alternate; dur: 3000; loop: 1"
+
+
+//var eventEl = sceneEl.querySelector('a-entity');
+/*
+entityEl.addEventListener('onclick', function () {
+    console.log('Entity collided with');
+});
+*/
+
+
+
 
 //ambientLight = document.createElement('a-entity');
 //directionalLight = document.createElement('a-entity');
 //ambientLight.setAttribute('light', {color: 'white', type: 'ambient', 'intensity': 2});
 
-lightAmb.intensity = 5;
+//lightAmb.intensity = 5;
 
 
 //pointLight = document.createElement('a-entity');
